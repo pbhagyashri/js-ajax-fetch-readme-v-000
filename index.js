8cfae7362ba6cb9354fa5ee8eba490e9fc423333
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-01-21 08:12:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+//28ef8af1576d871bf605bf985defd01f74b18810
+
+// fetch('https://api.github.com/user/repos').
+
+//     then(res => res.JSON()).
+//     then(json => console.log(json))
+    
+const token = '28ef8af1576d871bf605bf985defd01f74b18810'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
